fad561a627b88d8e01b8545e5c43c42e5c2cef5a
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-09-01 20:54:03 +0300

    render the ImgContainer component

---
+++ src/app/components/Gallery.tsx
@@ -0,0 +1,21 @@
+import fetchImages from "@/lib/fetchImages";
+import type { ImagesResults } from "@/models/Images";
+import ImgContainer from "./ImgContainer";
+
+const Gallery = async () => {
+  const url = "https://api.pexels.com/v1/curated";
+
+  const images: ImagesResults | undefined = await fetchImages(url);
+
+  if (!images)
+    return <h2 className="m-4 text-2xl font-bold">No Images Found!</h2>;
+  return (
+    <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
+      {images.photos.map((photo) => (
+        <ImgContainer key={photo.id} photo={photo} />
+      ))}
+    </section>
+  );
+};
+
+export default Gallery;
