99741c722911d517c304b6ce467fc81ef4fb108a
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-09-04 13:22:28 +0300

    make images more responsive

--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -7,8 +7,14 @@ interface ImgContainerProps {
 
 const ImgContainer = ({ photo }: ImgContainerProps) => {
   return (
-    <div className="h-64 bg-gray-200 rounded-xl">
-      <Image src={photo.src.large} alt={photo.alt} width={250} height={250} />
+    <div className="relative overflow-hidden h-64 bg-gray-200 rounded-xl group">
+      <Image
+        src={photo.src.large}
+        alt={photo.alt}
+        fill={true}
+        className="object-cover group-hover:opacity-75"
+        sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
+      />
     </div>
   );
 };
