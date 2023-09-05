2812f7277b7f4c8bfac48ccc6faf0f63f4cb1cd8
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-09-05 19:34:46 +0300

    loop url with blurred data

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,6 +1,7 @@
 import fetchImages from "@/lib/fetchImages";
 import type { ImagesResults } from "@/models/Images";
 import ImgContainer from "./ImgContainer";
+import addBlurredDataUrls from "@/lib/getBase64";
 
 const Gallery = async () => {
   const url = "https://api.pexels.com/v1/curated";
@@ -9,9 +10,12 @@ const Gallery = async () => {
 
   if (!images)
     return <h2 className="m-4 text-2xl font-bold">No Images Found!</h2>;
+
+  const photosWithBlur = await addBlurredDataUrls(images);
+
   return (
     <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
-      {images.photos.map((photo) => (
+      {photosWithBlur.map((photo) => (
         <ImgContainer key={photo.id} photo={photo} />
       ))}
     </section>
