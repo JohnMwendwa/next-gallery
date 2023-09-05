d81cb3a6f1ad1b5d74e4667e1f115ad3689fcaae
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-09-05 19:35:23 +0300

    add blurred urls to images

--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -12,6 +12,8 @@ const ImgContainer = ({ photo }: ImgContainerProps) => {
         src={photo.src.large}
         alt={photo.alt}
         fill={true}
+        placeholder="blur"
+        blurDataURL={photo.blurredDataUrl}
         className="object-cover group-hover:opacity-75"
         sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
       />
