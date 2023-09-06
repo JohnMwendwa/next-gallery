b005fe447b3c4ba166a9d8c35a6f5891682c830c
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-09-06 15:35:46 +0300

    create a Navbar component

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <header className="bg-black sticky top-0 z-10">
+      <nav className="flex flex-col gap-4 sm:flex-row sm:justify-between items-center p-4 font-bold max-w-6xl mx-auto text-white">
+        <h1 className="text-2xl  sm:text-3xl text-center whitespace-nowrap">
+          <Link href={"/"}>Next.js Image Gallery</Link>
+        </h1>
+      </nav>
+    </header>
+  );
+}
