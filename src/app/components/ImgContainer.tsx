5ada8766d880db33b30208d9ffca5b46f183ff17
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-09-07 14:57:47 +0300

    create a masonry grid gallery

--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -1,22 +1,38 @@
 import type { Photo } from "@/models/Images";
 import Image from "next/image";
+import Link from "next/link";
 
 interface ImgContainerProps {
   photo: Photo;
 }
 
 const ImgContainer = ({ photo }: ImgContainerProps) => {
+  const widthHeightRatio = photo.height / photo.width;
+  const galleryHeight = Math.ceil(250 * widthHeightRatio);
+  const photoSpan = Math.ceil(galleryHeight / 10) + 1;
   return (
-    <div className="relative overflow-hidden h-64 bg-gray-200 rounded-xl group">
-      <Image
-        src={photo.src.large}
-        alt={photo.alt}
-        fill={true}
-        placeholder="blur"
-        blurDataURL={photo.blurredDataUrl}
-        className="object-cover group-hover:opacity-75"
-        sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
-      />
+    <div
+      className="w-[250px] justify-self-center"
+      style={{ gridRow: `span ${photoSpan}` }}
+    >
+      <Link
+        href={photo.url}
+        target="_blank"
+        className="grid place-content-center"
+      >
+        <div className="overflow-hidden rounded-xl group">
+          <Image
+            src={photo.src.large}
+            alt={photo.alt}
+            width={photo.width}
+            height={photo.height}
+            placeholder="blur"
+            blurDataURL={photo.blurredDataUrl}
+            sizes="250px"
+            className="group-hover:opacity-75"
+          />
+        </div>
+      </Link>
     </div>
   );
 };
